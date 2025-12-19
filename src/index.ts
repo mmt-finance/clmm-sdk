7ee09321e0f02e8e031154bdcf722fd5262cabfe
Alex <wangzengyang66@gmail.com>
2025-12-19 21:26:32 +0800

    feat: support mmt for claim-routes & upgrade to 1.3.25

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { PositionModule } from './modules/positionModule';
 import * as Utils from './utils/poolUtils';
 import { TickMath } from './utils/math/tickMath';
 import * as Types from './types';
+import * as Constants from './constants/index';
 
-export { MmtSDK, PoolModule, PositionModule, Utils, TickMath, Types };
+export { MmtSDK, PoolModule, PositionModule, Utils, TickMath, Types, Constants };
